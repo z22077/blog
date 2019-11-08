512a42510cbc306886923ff8de3a68420e8d2c5a
z22077 <zx2207740110gmail.com>
2019-11-08 16:20:51 +0800

    ch8 no finished

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.article.title + '-' + str(self.id)
