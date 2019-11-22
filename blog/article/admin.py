fc3dc42d1987e4d549f8e2d3367eb4fab7a7358d
z22077 <zx2207740110gmail.com>
2019-11-22 16:26:45 +0800

    ch8

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from article.models import Article, Comment
 
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ['article', 'content', 'pubDateTime']
+    list_display_links = ['article']
+    list_filter = ['article', 'content']
+    search_fields = ['content']
+    list_editable = ['content']
+ 
+    class Meta:
+        model = Comment
+
 
 admin.site.register(Article)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentModelAdmin)
--- blog/article/models.py
+++ blog/article/models.py
@@ -4,14 +4,21 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=128, unique=True)
     content = models.TextField()
+    pubDateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+    class Meta:
+        ordering = ['-pubDateTime']
 
+   
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.CharField(max_length=128)
+    pubDateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.article.title + '-' + str(self.id)
+    class Meta:
+        ordering = ['pubDateTime']
